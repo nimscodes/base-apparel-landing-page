1e01754e7527368c3f175e6be847cd524400b4de
nimscodes <princenimako28@gmail.com>
2023-06-18 13:58:26 +0100

    modfications and footer done

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
-function validateForm(){
-    let input = document.forms["email-form"]["email"].value;
+
+    const email_form = document.querySelector('#email-form');
+    const input = document.getElementById('email');
     const error_message = document.querySelector('.error-message');
     const error_icon = document.querySelector('.error-icon');
     const form = document.querySelector('.form');
     const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if( input == "" || !input.match(emailPattern)){
-        error_message.style.display = "block";
-        error_icon.style.display = "block";
-        form.style.border = "2px solid var(--soft-red)";
-        return false;
-    }else{
-        alert(input);
-        return true;
-    }
+    email_form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        if (input.value == "" || !input.value.match(emailPattern)) {
+            error_message.style.display = "block";
+            error_icon.style.display = 'flex';
+            form.style.border = "2px solid var(--soft-red)";
+            input.value = "";
+        }else{
+            input.value = ""
+            alert("Email submitted");
+        }
+    })
+
     
-}
